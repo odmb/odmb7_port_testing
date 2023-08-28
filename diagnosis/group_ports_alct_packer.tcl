001708516399749abd3f0d51952395c03f37889a
Jaebak Kim <jbkim@ucsb.edu>
2023-08-28 15:12:15 -0700

    Added helper files

---
+++ diagnosis/group_ports_alct_packer.tcl
@@ -0,0 +1,9 @@
+# Run by source group_ports.tcl
+create_hw_probe -m probe0[13:0]      alct_rec0[13:0]     [get_hw_ilas hw_ila_9]
+create_hw_probe -m probe0[27:14]     alct_rec1[13:0]     [get_hw_ilas hw_ila_9]
+create_hw_probe -m probe0[41:28]     alct_rec2[13:0]     [get_hw_ilas hw_ila_9]
+create_hw_probe -m probe0[55:42]     alct_rec3[13:0]     [get_hw_ilas hw_ila_9]
+create_hw_probe -m probe0[69:56]     alct_rec4[13:0]     [get_hw_ilas hw_ila_9]
+create_hw_probe -m probe0[83:70]     alct_rec5[13:0]     [get_hw_ilas hw_ila_9]
+create_hw_probe -m probe0[97:84]     alct_rec6[13:0]     [get_hw_ilas hw_ila_9]
+create_hw_probe -m probe0[111:98]    alct_rec7[13:0]     [get_hw_ilas hw_ila_9]
